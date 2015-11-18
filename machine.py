ee58b29c059769cac0dd00bf46286e37ad03b263
Carl Wingardh <c.a.wingardh@gmail.com>
2015-11-18 20:37:11 +0100

    Finished paralellization. Complete with locks

--- machine.py
+++ machine.py
@@ -3,37 +3,40 @@
 from lxml import html
 import requests
 import random
+import threading
 from threading import Thread
 
 def scrape_quotes_on_page(i):
     global quotes
-
+    global quote_lock
+    
     website = 'http://danryckertjustsaid.tumblr.com/page/' + str(i)
 
     page = requests.get(website)
     tree = html.fromstring(page.content)
 
     quotes_on_this_page = tree[1].xpath('//article[@class="post text"]//h3//a/text()')
+
+    quote_lock.acquire()
     quotes = quotes + quotes_on_this_page
+    quote_lock.release()
     
 
 print 'Welcome to The Dan Ryckert Quote Machine'
 
 quotes = []
 
-pages_to_scrape = 10
+pages_to_scrape = 30
 
-threads = [pages_to_scrape]
+quote_lock = threading.RLock()
+threads = []
 
-for i in range(len(threads)):
-    threads[i] = Thread(target=scrape_quotes_on_page, args=(i+1,))
+for i in range(pages_to_scrape):
+    threads.append(Thread(target=scrape_quotes_on_page, args=(i+1,)))
     threads[i].start()
 
-for i in range(len(threads)):
+for i in range(pages_to_scrape):
     threads[i].join()
-
-print quotes
     
 random.seed()
 print random.choice(quotes)
-
