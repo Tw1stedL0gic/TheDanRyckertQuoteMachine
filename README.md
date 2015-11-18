2041cd5c76669d1ec72407b7a38304227fa23667
Carl Wingardh <c.a.wingardh@gmail.com>
2015-11-18 16:42:34 +0100

    Added threads but it only scrapes one page :/

---
+++ README.md
@@ -0,0 +1,9 @@
+Welcome to the Dan Ryckert Quote Machine
+
+
+
+Before Threading:
+
+real 4.28
+user 0.22
+sys 0.03
--- machine.py
+++ machine.py
@@ -3,26 +3,36 @@
 from lxml import html
 import requests
 import random
+from threading import Thread
 
 def scrape_quotes_on_page(i):
+    global quotes
+
     website = 'http://danryckertjustsaid.tumblr.com/page/' + str(i)
 
     page = requests.get(website)
     tree = html.fromstring(page.content)
 
     quotes_on_this_page = tree[1].xpath('//article[@class="post text"]//h3//a/text()')
-
-    return quotes_on_this_page
-
+    quotes = quotes + quotes_on_this_page
+    
 
 print 'Welcome to The Dan Ryckert Quote Machine'
 
 quotes = []
 
-for i in range(1, 10):
-    quotes_on_this_page = scrape_quotes_on_page(i)
-    if len(quotes_on_this_page):
-        quotes = quotes + quotes_on_this_page
+pages_to_scrape = 10
+
+threads = [pages_to_scrape]
+
+for i in range(len(threads)):
+    threads[i] = Thread(target=scrape_quotes_on_page, args=(i+1,))
+    threads[i].start()
+
+for i in range(len(threads)):
+    threads[i].join()
+
+print quotes
     
 random.seed()
 print random.choice(quotes)
